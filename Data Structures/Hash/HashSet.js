734e3e085c770ddfd40951d342abd9f9593d7c1f
Will Germany <germany.william@gmail.com>
2018-03-16 12:14:27 -0400

    Added HashSet tests

--- Data Structures/Hash/HashSet.js
+++ Data Structures/Hash/HashSet.js
@@ -3,14 +3,28 @@ export default class HashSet {
         this.storage = {};
     }
 
+    /**
+     * Adds a key to the set.
+     * 
+     * @param {any} key 
+     * @memberof HashSet
+     */
     add (key) {
         if (!key) {
             throw new Error("IllegalArgumentException"); 
         } 
 
         this.storage[key] = true;
+        return this;
     }
 
+    /**
+     * Checks to see if the key exists in the set.
+     * 
+     * @param {any} key 
+     * @returns 
+     * @memberof HashSet
+     */
     has (key) {
         if (!key) {
             throw new Error("IllegalArgumentException"); 
@@ -21,27 +35,63 @@ export default class HashSet {
         }
     }
 
+    /**
+     * Removes a key from the set if it exists.
+     * 
+     * @param {any} key 
+     * @memberof HashSet
+     */
     remove (key) {
         if (!key) {
             throw new Error("IllegalArgumentException"); 
         } else if (this.has(key)) {
             delete this.storage[key];
+            return this;
         }
     }
 
+    /**
+     * Utility method to convert the set to an array.
+     * 
+     * @returns 
+     * @memberof HashSet
+     */
     toArray () {
         return Object.keys(this.storage);
     }
 
-    unionWith () {
-        
+    /**
+     * Combines multiple sets into one.
+     * 
+     * @param {object[]} set 
+     * @memberof HashSet
+     */
+    unionWith (...set) {
+        if (typeof set === 'object') {
+            set.map(setObj => Object.assign(this.storage, setObj.storage));
+            return this;
+        } else {
+            throw new Error("IllegalArgumentException"); 
+        }
     }
 
+    /**
+     * Gets the size of the set.
+     * 
+     * @returns 
+     * @memberof HashSet
+     */
     size () {
         return Object.keys(this.storage).length;
     }
-
+    
+    /**
+     * Removes all keys from the set.
+     * 
+     * @memberof HashSet
+     */
     clear () {
         this.storage = {};
+        return this;
     }
 }
--- Data Structures/Hash/HashSet.spec.js
+++ Data Structures/Hash/HashSet.spec.js
@@ -8,31 +8,64 @@ describe('Hash Set', () => {
     });
 
     it ('Add Method', () => {
-
+        const hashSet = new HashSet();
+        hashSet.add("Bob");
+        hashSet.add("Joe");
+        const result = hashSet.size();
+        expect(result).toBe(2);
     });
 
     it ('Has Method', () => {
-
+        const hashSet = new HashSet();
+        hashSet.add("Bob");
+        hashSet.add("Joe");
+        const result = hashSet.has("Bob");
+        expect(result).toBe(true);
     });
 
     it ('Remove Method', () => {
-
+        const hashSet = new HashSet();
+        hashSet.add("Bob");
+        hashSet.remove("Bob");
+        const result = hashSet.has("Bob");
+        expect(result).toBe(false);
     });
 
     it ('ToArray Method', () => {
-
+        const hashSet = new HashSet();
+        hashSet.add("Bob");
+        hashSet.add("Joe");
+        const result = hashSet.toArray();
+        expect(result).toEqual(["Bob", "Joe"]);
     });
 
     it ('Size Method', () => {
-
+        const hashSet = new HashSet();
+        hashSet.add("Bob");
+        hashSet.add("Joe");
+        const result = hashSet.size();
+        expect(result).toBe(2);
     });
 
     it ('Clear Method', () => {
-
+        const hashSet = new HashSet();
+        hashSet.add("Bob");
+        hashSet.add("Joe");
+        hashSet.add("Mary");
+        hashSet.clear();
+        const result = hashSet.size();
+        expect(result).toBe(0);
     });
 
     it ('UnionWith Method', () => {
-
+        const hashSet = new HashSet();
+        hashSet.add("Bob");
+        const hS = new HashSet();
+        hS.add("Joey");
+        const hashS = new HashSet();
+        hashS.add("Mom");
+        const result = hashSet.unionWith(hS, hashS).toArray();
+        expect(result).toEqual(["Bob", "Joey", "Mom"]);
     });
 
 });
